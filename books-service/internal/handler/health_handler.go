0f43f3e6e60ca5309aa33517b57c607fbf556dab
rubanik.roman <rubanik.roman@wb.ru>
2026-08-09 13:13:40 +0300

    [books service] [auth service] add timeout for db in health handler

--- books-service/internal/handler/health_handler.go
+++ books-service/internal/handler/health_handler.go
@@ -57,6 +57,9 @@ func (h *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *HealthHandler) checkDatabase(ctx context.Context) Check {
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
 	now := time.Now()
 	err := h.db.PingContext(ctx)
 	after := time.Since(now)
