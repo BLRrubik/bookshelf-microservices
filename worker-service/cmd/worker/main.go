450ffae7a6629d90ca3e0c291db39a136bef8e15
rubanik.roman <rubanik.roman@wb.ru>
2026-08-10 20:56:59 +0300

    [worker service] add consumer

--- worker-service/cmd/worker/main.go
+++ worker-service/cmd/worker/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"bookshelf/worker-service/internal/config"
+	"bookshelf/worker-service/internal/queue"
+	"fmt"
+	"log"
 	"log/slog"
+	"os"
+	"os/signal"
 )
 
 func main() {
@@ -11,5 +16,23 @@ func main() {
 	slog.Info("Loading configuration...")
 	cfg := config.Load()
 
-	_ = cfg
+	consumer, err := queue.NewConsumer(cfg.RabbitMQURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer consumer.Close()
+
+	consumer.RegisterHandler("test_queue", func(body []byte) error {
+		fmt.Println(string(body))
+
+		return nil
+	})
+
+	consumer.Start()
+
+	term := make(chan os.Signal, 1)
+
+	signal.Notify(term, os.Interrupt)
+
+	<-term
 }
---
+++ worker-service/internal/queue/consumer.go
@@ -0,0 +1,87 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type HandlerFunc func(body []byte) error
+
+type Consumer struct {
+	conn     *amqp.Connection
+	channel  *amqp.Channel
+	handlers map[string]HandlerFunc
+}
+
+func NewConsumer(url string) (*Consumer, error) {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		conn:     conn,
+		channel:  ch,
+		handlers: make(map[string]HandlerFunc),
+	}, nil
+}
+
+func (c *Consumer) RegisterHandler(queue string, handler HandlerFunc) {
+	c.handlers[queue] = handler
+}
+
+func (c *Consumer) Start() error {
+	for queue, handler := range c.handlers {
+		if err := c.channel.Qos(1, 0, false); err != nil {
+			return err
+		}
+
+		deliveries, err := c.channel.ConsumeWithContext(
+			context.Background(),
+			queue,
+			"",
+			false,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			return err
+		}
+
+		go c.consume(queue, handler, deliveries)
+	}
+
+	return nil
+}
+
+func (c *Consumer) consume(queue string, handler HandlerFunc, msgs <-chan amqp.Delivery) {
+	for msg := range msgs {
+		if err := handler(msg.Body); err != nil {
+			slog.Error(fmt.Sprintf("Failed to handle message from queue %s: %s", queue, err))
+		}
+
+		if err := msg.Ack(true); err != nil {
+			slog.Error(fmt.Sprintf("Failed to acknowledge message from queue %s: %s", queue, err))
+		}
+	}
+}
+
+func (c *Consumer) Close() error {
+	if err := c.channel.Close(); err != nil {
+		_ = c.conn.Close()
+
+		return err
+	}
+
+	return c.conn.Close()
+}
