422825d008c292bcc204864538feef2bacaa3e6e
rubanik.roman <rubanik.roman@wb.ru>
2026-08-13 16:04:39 +0300

    [worker service] add handler for image

--- worker-service/go.mod
+++ worker-service/go.mod
@@ -3,6 +3,14 @@ module bookshelf/worker-service
 go 1.26
 
 require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/minio/minio-go/v7 v7.2.1
+	github.com/rabbitmq/amqp091-go v1.13.0
+	go.uber.org/zap v1.27.0
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -11,14 +19,14 @@ require (
 	github.com/klauspost/crc32 v1.3.0 // indirect
 	github.com/minio/crc64nvme v1.1.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
-	github.com/minio/minio-go/v7 v7.2.1 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
-	github.com/rabbitmq/amqp091-go v1.13.0 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/tinylib/msgp v1.6.1 // indirect
 	github.com/zeebo/xxh3 v1.1.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.51.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
---
+++ worker-service/internal/handler/image_handler.go
@@ -0,0 +1,93 @@
+package handler
+
+import (
+	"bookshelf/worker-service/internal/queue"
+	"bookshelf/worker-service/internal/storage"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"log/slog"
+
+	"github.com/disintegration/imaging"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+const (
+	CoverWidth  = 400
+	CoverHeight = 600
+	ThumbWidth  = 100
+	ThumbHeight = 150
+	JPEGQuality = 85
+)
+
+type ImageCompressMessage struct {
+	BookID       string `json:"book_id"`
+	CoverID      string `json:"cover_id"`
+	OriginalPath string `json:"original_path"`
+}
+
+type ImageHandler struct {
+	storage *storage.MinIOStorage
+	db      *sqlx.DB
+}
+
+func (h *ImageHandler) HandleImageCompress(body []byte) error {
+	ctx := context.Background()
+	var msg ImageCompressMessage
+	if err := json.Unmarshal(body, &msg); err != nil {
+		slog.Error("Failed to unmarshal image compress message", zap.String("body", string(body)), zap.Error(err))
+
+		return err
+	}
+
+	fileBytes, err := h.storage.GetFile(ctx, msg.OriginalPath)
+	if err != nil {
+		slog.Error("Failed to get file", zap.String("original_path", msg.OriginalPath), zap.Error(err))
+
+		return fmt.Errorf("%w, failed to get file: %w", queue.ErrTemporary, err)
+	}
+
+	img, err := imaging.Decode(bytes.NewReader(fileBytes))
+	if err != nil {
+		slog.Error("Failed to decode image", zap.String("original_path", msg.OriginalPath), zap.Error(err))
+
+		return err
+	}
+
+	if err = h.processImage(ctx, img, CoverWidth, CoverHeight, JPEGQuality, fmt.Sprintf("/covers/%s/cover.jpg", msg.BookID)); err != nil {
+		slog.Error("Failed to process image", zap.String("original_path", msg.OriginalPath), zap.Error(err))
+
+		return err
+	}
+
+	if err = h.processImage(ctx, img, ThumbWidth, ThumbHeight, JPEGQuality, fmt.Sprintf("/covers/%s/thumb.jpg", msg.BookID)); err != nil {
+		slog.Error("Failed to process image", zap.String("original_path", msg.OriginalPath), zap.Error(err))
+
+		return err
+	}
+
+	return nil
+}
+
+func (h *ImageHandler) processImage(ctx context.Context, img image.Image, width, height, quality int, objectName string) error {
+	data := imaging.Fill(img, ThumbWidth, ThumbHeight, imaging.Center, imaging.Lanczos)
+
+	var buf bytes.Buffer
+	if err := jpeg.Encode(&buf, data, &jpeg.Options{Quality: JPEGQuality}); err != nil {
+		slog.Error("Failed to encode image", zap.Error(err))
+
+		return err
+	}
+
+	if err := h.storage.UploadFile(ctx, objectName, buf.Bytes(), "image/jpg"); err != nil {
+		slog.Error("Failed to upload image", zap.String("object_name", objectName), zap.Error(err))
+
+		return err
+	}
+
+	return nil
+}
