566a074750456aea9c07e7ebc19ff2639091c63f
rubanik.roman <rubanik.roman@wb.ru>
2026-08-16 00:28:54 +0300

    [gateway] code review fix

--- api-gateway/internal/middleware/request_id.go
+++ api-gateway/internal/middleware/request_id.go
@@ -11,12 +11,12 @@ func RequestID() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			requestID := r.Header.Get("X-Request-Id")
-			ctx := r.Context()
 			if requestID == "" {
 				requestID = uuid.NewString()
-				r.Header.Set("X-Request-Id", requestID)
-				ctx = context.WithValue(ctx, "request_id", requestID)
 			}
+
+			r.Header.Set("X-Request-Id", requestID)
+			ctx := context.WithValue(r.Context(), "request_id", requestID)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
